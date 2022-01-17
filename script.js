5c8146939146cfc3d788c045a29a02e7f8503a44
Mari-Artho <mari.artho@medieinstitutet.se>
2022-01-17 15:06:35 +0100

    If there is a letter in the box, it will not change.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var currentPlayer = "O";
 
 function place(box){
+    //If there is a letter in the box, it will not change
+    if(box.innerText !="") return;
+    //If the box is "current player", enter a different value
     box.innerText = currentPlayer;
     if(currentPlayer == "O"){
         currentPlayer = "X";
