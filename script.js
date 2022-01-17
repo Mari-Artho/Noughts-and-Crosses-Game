d0137a275dbb212570571cbde00db370977317af
Mari-Artho <mari.artho@medieinstitutet.se>
2022-01-17 15:10:10 +0100

    Create ternary operator(if).

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function place(box){
     //If there is a letter in the box, it will not change
     if(box.innerText !="") return;
     //If the box is "current player", enter a different value
-    box.innerText = currentPlayer;
-    if(currentPlayer == "O"){
-        currentPlayer = "X";
-    }else{
-        currentPlayer = "O";
-    }
+    // box.innerText = currentPlayer;
+    // if(currentPlayer == "O"){
+    //     currentPlayer = "X";
+    // }else{
+    //     currentPlayer = "O";
+    // }
+    //row 6-12 and row 14 is same.
+    currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
 }
