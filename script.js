87c95c13565977f699052547c2a8467de879b004
Mari-Artho <mari.artho@medieinstitutet.se>
2022-01-17 16:11:18 +0100

    Create function checkWinner.

--- script.js
+++ script.js
@@ -10,11 +10,29 @@ function place(box){
 }
 
 function checkGameBoard(){
-    var first = document.getElementById("0_0").innerText;
-    var second = document.getElementById("1_0").innerText;
-    var third = document.getElementById("2_0").innerText;
-    if(first == "") return;
-        if(first==second && first == third){
-            alert("You win!");
-        }
+    //Horizontal
+   for(var i=0; i<=2; i++){
+    // var first = document.getElementById("0_" +i).innerText;
+    // var second = document.getElementById("1_" +i).innerText;
+    // var third = document.getElementById("2_" +i).innerText;
+    //checkWinner(first,second,third);
+    checkWinner(document.getElementById("0_" +i).innerText,document.getElementById("1_" +i).innerText, document.getElementById("2_" +i).innerText)
+    } 
+
+    //Vertical
+    for(var i=0; i<=2; i++){
+    checkWinner(document.getElementById( i+ "_0").innerText,document.getElementById(i+ "_1").innerText,document.getElementById(i+ "_2").innerText);
+    }
+
+    //Diagonal-1
+    checkWinner(document.getElementById("0_0").innerText,document.getElementById("1_1").innerText,document.getElementById("2_2").innerText);
+
+    //Diagonal-2
+    checkWinner(document.getElementById("0_2").innerText,document.getElementById("1_1").innerText,document.getElementById("2_0").innerText);
+}
+
+function checkWinner(first,second,third){
+    if(first != "" && first==second && first==third){
+        alert("You are amazing!");
+    }
 }
