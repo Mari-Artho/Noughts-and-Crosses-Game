10e3ee624b5c44a8908f9f743c62e0dc8bfaae80
Mari-Artho <mari.artho@medieinstitutet.se>
2022-01-17 15:34:39 +0100

    function checkGameBoard, only one row.

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function place(box){
     //If there is a letter in the box, it will not change
     if(box.innerText !="") return;
     //If the box is "current player", enter a different value
-    // box.innerText = currentPlayer;
-    // if(currentPlayer == "O"){
-    //     currentPlayer = "X";
-    // }else{
-    //     currentPlayer = "O";
-    // }
-    //row 6-12 and row 14 is same.
+    box.innerText = currentPlayer;
     currentPlayer == "O" ? currentPlayer = "X" : currentPlayer = "O";
+    checkGameBoard();
+}
+
+function checkGameBoard(){
+    var first = document.getElementById("0_0").innerText;
+    var second = document.getElementById("1_0").innerText;
+    var third = document.getElementById("2_0").innerText;
+    if(first == "") return;
+        if(first==second && first == third){
+            alert("You win!");
+        }
 }
